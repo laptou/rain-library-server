2e40742a693371769872592ed4ca60020074f1ba
Ibiyemi Abiodun <ibiyemi@intulon.com>
2018-01-16 11:53:06 -0500

    Added some tslinting

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 if (process.env.NODE_ENV === "development")
     exports.db = "mongodb://192.168.3.173/library";
-else
-    exports.db = "mongodb://localhost/library";
+// else
+exports.db = "mongodb://localhost/library";
 //# sourceMappingURL=config.js.map
