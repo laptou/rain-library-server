d7b370868783d759a0841bed83cd162dd0ed50db
Ibiyemi Abiodun <ibiyemi@intulon.com>
2018-05-09 16:54:41 -0400

    fixed publish scripts

--- script/publish-prod.ps1
+++ script/publish-prod.ps1
@@ -1,2 +1,3 @@
+set-location ..
 & tsc
 & robocopy ./ "\\192.168.2.202\ibiyemi.intulon.com\server" *.js *.json /e /xd .* node_modules data /xo /xf tsconfig.json tslint.json config.js
--- script/publish.ps1
+++ script/publish.ps1
@@ -1,2 +1,3 @@
+set-location ..
 & tsc
 & robocopy ./ "\\192.168.3.173\Node Server\server" *.js *.json /e /xd .* node_modules data /xo /xf tsconfig.json tslint.json
